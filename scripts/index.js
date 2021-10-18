a8ec5c93d610e3e6be5e38a7dd04f051d3254399
Amidamaru2199 <sarmathoz1999@gmail.com>
2021-10-18 12:45:56 +0300

    На проверку

--- scripts/index.js
+++ scripts/index.js
@@ -15,11 +15,28 @@ popupCloseButtonElement.addEventListener('click', closePopup);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Находим форму в DOM
-const formElement = document.querySelector(.) // Воспользуйтесь методом querySelector()
+const formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+
 // Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
+const nameInput = formElement.querySelector('.popup__field-name'); // Воспользуйтесь инструментом .querySelector()
+const jobInput = formElement.querySelector('.popup__field-profession');// Воспользуйтесь инструментом .querySelector()
+
+const nameElement = document.querySelector('.profile__name');
+const descriptionElement = document.querySelector('.profile__description');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -29,14 +46,35 @@ function formSubmitHandler (evt) {
                                                 // О том, как это делать, расскажем позже.
 
     // Получите значение полей jobInput и nameInput из свойства value
-
+    
     // Выберите элементы, куда должны быть вставлены значения полей
-
+nameElement.textContent = nameInput.value;
+descriptionElement.textContent = jobInput.value;
     // Вставьте новые значения с помощью textContent
+
 }
 
+formElement.addEventListener('submit', formSubmitHandler);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
+
+
+const popupAddEndCloseButton = document.querySelector('.popup__button');
+popupAddEndCloseButton.addEventListener('click', closePopup);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
