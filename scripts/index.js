55b22f14d5ca09090056d761a67d2ed3cfdc674e
Amidamaru <hoztovari99@yandex.ru>
2021-10-19 19:48:42 +0300

    проверка2

--- scripts/index.js
+++ scripts/index.js
@@ -15,70 +15,25 @@ popupCloseButtonElement.addEventListener('click', closePopup);
 
 
 
+const formElement = document.querySelector('.popup__form');
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Находим форму в DOM
-const formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
-
-// Находим поля формы в DOM
-const nameInput = formElement.querySelector('.popup__field-name'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement.querySelector('.popup__field-profession');// Воспользуйтесь инструментом .querySelector()
+const nameInput = formElement.querySelector('.popup__field-name');
+const jobInput = formElement.querySelector('.popup__field-profession');
 
 const nameElement = document.querySelector('.profile__name');
 const descriptionElement = document.querySelector('.profile__description');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
+    evt.preventDefault();
     
-    // Выберите элементы, куда должны быть вставлены значения полей
 nameElement.textContent = nameInput.value;
 descriptionElement.textContent = jobInput.value;
-    // Вставьте новые значения с помощью textContent
-
-}
+};
 
 formElement.addEventListener('submit', formSubmitHandler);
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-
 
 const popupAddEndCloseButton = document.querySelector('.popup__button');
 popupAddEndCloseButton.addEventListener('click', closePopup);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*const vectorElement = document.querySelector('.element__vector');
